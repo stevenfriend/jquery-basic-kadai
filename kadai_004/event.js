17fac4b86eab471ec55e1c5a41eb41f415799491
Steven Friend <stevenmarkfriend@gmail.com>
2024-01-05 21:52:49 +0900

    周囲の無名関数を削除

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,5 +1,3 @@
-$(function() {
-  $(window).on('load scroll', e => {
-    console.log(`${e.type}イベントが発生しました`);
-  })
-});
+$(window).on('load scroll', e => {
+  console.log(`${e.type}イベントが発生しました`);
+})
